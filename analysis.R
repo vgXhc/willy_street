874b11303e078ab9f8254ef9986365dd9b4ebe07
Harald Kliems <kliems@gmail.com>
2025-09-13 14:59:00 -0500

    improved chart

--- analysis.R
+++ analysis.R
@@ -1,3 +1,13 @@
+library(pins)
+library(tidyverse)
+
+board <- board_s3("willy2", 
+                  region = "us-east-1", 
+                  access_key = Sys.getenv("S3_WILLY_KEY"), 
+                  secret_access_key = Sys.getenv("S3_WILLY_SECRET"))
+
+full_routes_pre <- pin_read(board, "full_routes")
+
 full_routes_pre |> 
   filter(!(origin == "JND_at_North_Shore" & destination == "Willy_at_Ingersoll")) |> 
   mutate(route_id = case_when(
@@ -18,13 +28,39 @@ full_routes_pre |>
       intermediate == "Willy_at_Ingersoll" ~ "Milwaukee to JND via Willy",
     destination == "E_Wash_at_Milwaukee" &
       origin == "JND_at_North_Shore" &
-      intermediate == "Willy_at_Ingersoll" ~ "JND to Milwaukee via Willy"),
-    duration = as.integer(str_remove(duration, "s"))
+      intermediate == "Willy_at_Ingersoll" ~ "JND to Milwaukee via Willy",
+    destination == "Wilson_at_Willy" &
+      origin == "Eastwood_at_Winnebago" &
+      intermediate == "Willy_at_Ingersoll" ~ "Eastwood to Hairball",
+    origin == "Wilson_at_Willy" &
+      destination == "Eastwood_at_Winnebago" &
+      intermediate == "Willy_at_Ingersoll" ~ "Hairball to Eastwood"),
+    duration = as.integer(str_remove(duration, "s")),
+    static_duration = as.integer(str_remove(static_duration, "s")),
+    traffic_delay = duration - static_duration,
+    direction = case_match(route_id,
+                           c("JND to Olbrich",
+                             "JND to Milwaukee via E Wash",
+                             "JND to Milwaukee via Willy",
+                             "Hairball to Eastwood") ~ "EB",.default = "WB"),
+    rush_hour = case_when(
+      hour(request_time) == 7 | (hour(request_time) == 8 & minute(request_time) <= 30) ~ "am",
+      hour(request_time) == 16 | (hour(request_time) == 17 & minute(request_time) <= 30) ~ "pm",
+      .default = NA)
   ) |> 
   filter(!is.na(route_id)) |> 
-  ggplot(aes(request_time, duration, color = route_id)) +
+  ggplot(aes(request_time, traffic_delay, color = rush_hour)) +
   geom_point() +
-  facet_wrap(~ route_id) +
-  theme(legend.position = "none")
+  #gghighlight::gghighlight(rush_hour %in% c("am", "pm")) +
+  facet_wrap(vars(direction, route_id), nrow = 2) +
+  theme() +
+  hrbrthemes::theme_ipsum() +
+  xlab(element_blank()) +
+  ylab("Traffic delay (seconds)") +
+  labs(
+    title = "Traffic delay on routes through the Madison Isthmus",
+    subtitle = "Deviation of current projected from historical travel time",
+    caption = "Data: Google Routes API"
+  )
 
   
